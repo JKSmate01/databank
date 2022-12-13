2a15e019d68a2e4f7ea67af7dcb395c2756b3f10
Jakus Mate <jakus.mate.2021e@ms.gdszeged.hu>
2022-12-13 13:33:16 +0100

    20221213

--- katona(class).py
+++ katona(class).py
@@ -1,4 +1,4 @@
-class hires_katona:
+class Hires_katona:
     def __init__(self,nev,foglalkozas,nemzetiseg):
         self.nev = nev
         self.foglalkozas = foglalkozas
@@ -13,7 +13,7 @@ for i in range(3):
     nev = input("Add meg egy híres katona nevét! ")
     fog = input("Add meg a rangját! ")
     n = input("Add meg a nemzetiségét (o/n)! ")
-    hk.append(hires_katona(nev,fog,n))
+    hk.append(Hires_katona(nev,fog,n))
 for katona in hk:
     print(f"{katona.elotag()} {katona.nev} egy híres {katona.nemzetiseg}")
 
